eb09e2d6aa354a996773a254a2e93740b04cd52f
Leonardo Capoccia <leonardo.capoccia@hotmail.it>
2021-08-24 18:30:54 +0200

    Removed useless import

--- src/app/pages/exercise-list/exercise-list.page.ts
+++ src/app/pages/exercise-list/exercise-list.page.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ExerciseService } from 'src/app/services/exercise.service';
 
